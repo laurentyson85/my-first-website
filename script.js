5484e0808878b9a5f89342f67b14a03a512251ea
Lauren Tyson <lauren.wlkr@gmail.com>
2022-05-03 16:27:11 -0600

    made a click button

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ subHeading.id = "pageSubHeader"
 //subHeading.textContent = "The beginning of a bright-eyed coder's journey..."
 //document.querySelector("body").appendChild(subHeading)
 //subHeading.style.fontStyle = "italic"
+
+let btnCounter = document.getElementById(buttonCounter)
+let counter = 0
+
+btnCounter.addEventListener("click", function(){
+    while(counter < 10) {
+        counter++
+        console.log(`Lauren has a cool counter, watch it${counter}`)
+        counter = counter + 1
+    }    
+})
