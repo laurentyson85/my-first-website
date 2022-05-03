4e7989b6f4c676195b13fde71dffe80289972934
Lauren Tyson <lauren.wlkr@gmail.com>
2022-05-03 09:53:58 -0600

    moved subheader JavaScript code to be HTML instead

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const subHeading = document.createElement("h4")
 subHeading.id = "pageSubHeader"
-subHeading.textContent = "The beginning of a bright-eyed coder's journey..."
-document.querySelector("body").appendChild(subHeading)
-subHeading.style.fontStyle = "italic"
+//created the below code in HTML instead
+//subHeading.textContent = "The beginning of a bright-eyed coder's journey..."
+//document.querySelector("body").appendChild(subHeading)
+//subHeading.style.fontStyle = "italic"
